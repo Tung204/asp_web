175e00c280bf3dad2bcca33e9ce90756e1f2d064
Tung204 <nst874@gmail.com>
2024-09-06 14:34:41 +0700

    Create BaiTap04 & BaiTap05

---
+++ BaiTap05/BaiTap05/Controllers/TheLoaiController.cs
@@ -0,0 +1,25 @@
+﻿using BaiTap05.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTap05.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.ngay = "Ngày 1";
+            ViewBag.thang = "Tháng 2";
+            ViewData["Nam"] = "Năm 2030";
+            return View();
+        }
+        public IActionResult Index2()
+        {
+            var theloai = new TheLoaiViewModel
+            {
+                Id = 1,
+                Name = "Trinh Tham"
+            };
+            return View(theloai);
+        }
+    }
+}
---
+++ BaiTap05/BaiTap05/Models/TheLoaiViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTap05.Models
+{
+    public class TheLoaiViewModel : Controller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
