987244863526b9a6da17c25781fdac259bc0201e
Tung204 <nst874@gmail.com>
2024-09-09 14:41:26 +0700

    add BaiTap06

---
+++ Baitap06/Baitap06/Models/TaiKhoanViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Baitap06.Models
+{
+    public class TaiKhoanViewModel
+    {
+        public int Id {  get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
