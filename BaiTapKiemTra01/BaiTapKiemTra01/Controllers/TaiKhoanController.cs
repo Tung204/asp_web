cd08adaf877c38529a094eb364f4c76d2a481f9b
Tung204 <nst874@gmail.com>
2024-09-10 14:53:05 +0700

    Create BaiTapKiemTra01

---
+++ BaiTapKiemTra01/BaiTapKiemTra01/Controllers/TaiKhoanController.cs
@@ -0,0 +1,32 @@
+﻿using BaiTapKiemTra01.Models;
+using Microsoft.AspNetCore.Mvc;
+using NuGet.Common;
+
+namespace BaiTapKiemTra01.Controllers
+{
+    public class TaiKhoanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult DangKy(TaiKhoanViewModel taiKhoan)
+        {
+            if (taiKhoan.Username != null)
+            {
+                return Content(taiKhoan.Username);
+            }
+            return View(taiKhoan);
+        }
+        public IActionResult BaiTap2()
+        {
+            var sanpham = new SanPhamViewModel()
+            {
+                TenSP = "Trà",
+                GiaBan = 1000,
+                AnhMoTa = "/images/p1.jpg"
+            };
+            return View(sanpham);
+        }
+    }
+}
---
+++ BaiTapKiemTra01/BaiTapKiemTra01/Models/TaiKhoanViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BaiTapKiemTra01.Models
+{
+    public class TaiKhoanViewModel
+    {
+        public string Username { get; set; }
+        public int Password { get; set; }
+        public string Fullname { get; set; }
+        public int Age { get; set; }
+    }
+}
